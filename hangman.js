f1c78908d1952eaa1815ba227162cdce02718a3d
manoj1202 <manoj.appalaneni@gmail.com>
2018-05-04 14:06:01 -0500

    Game

---
+++ hangman.js
@@ -0,0 +1,25 @@
+const Hangman = function(word  = [], guesses){
+    this.word = word.toLowerCase().split('');
+    this.guesses = guesses;
+    this.gussedLetters = ['c'];
+}
+
+Hangman.prototype.getPuzzel = function(){
+    let puzzel = '';
+
+    this.word.forEach((letter) => {
+        if(this.gussedLetters.includes(letter) || letter === ' ')
+        {
+             puzzel +=  letter
+        } else {
+            puzzel +=  '*';
+        }
+    })
+    return puzzel;
+}
+
+
+const match = new Hangman('cat', 2); 
+console.log(match.getPuzzel());
+const word2 = new Hangman('New Jersy', 4);
+ console.log(word2.getPuzzel());
